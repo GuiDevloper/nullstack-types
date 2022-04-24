7c5ce403307cf85e88cf0936ed52aca4a1fc279a
GuiDevloper <guicorrea384@gmail.com>
2022-04-24 19:10:37 -0300

    :label: Becoming a mirror to nullstack built-in types

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,3 @@
-/// <reference path="./Context/index.d.ts"/>
-/// <reference path="./Plugin.d.ts"/>
+/// <reference path="./types/index.d.ts"/>
 
-interface indexContext extends Context {
-  start()
-}
-
-declare module 'nullstack' {
-  export default class Nullstack {
-    static start?(App: any): indexContext
-    static use?(Plugin: ElementPlugin): undefined
-  }
-}
-
-declare module '*.njs' {
-  export default class {}
-}
+export * from './types';
---
+++ types/index.d.ts
@@ -0,0 +1,23 @@
+import { NullstackClientContext } from './ClientContext'
+import { NullstackPlugin } from './Plugin'
+import { NullstackServerContext } from './ServerContext'
+
+export * from './ClientContext'
+export * from './Environment'
+export * from './Page'
+export * from './Params'
+export * from './Plugin'
+export * from './Project'
+export * from './Router'
+export * from './Secrets'
+export * from './Self'
+export * from './Server'
+export * from './ServerContext'
+export * from './Settings'
+export * from './Worker'
+
+export default class Nullstack<Type = any> {
+  constructor(props?: Type)
+  static start?(App: any): NullstackClientContext | NullstackServerContext
+  static use?(Plugin: NullstackPlugin)
+}
